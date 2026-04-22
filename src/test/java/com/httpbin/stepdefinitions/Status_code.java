90e4529cac250582c08f6dfa6d27c324964321a8
Mahitha <smgmm2004@gmail.com>
2026-04-22 15:33:06 +0530

    altered excel data and used it through utility

--- src/test/java/com/httpbin/stepdefinitions/Status_code.java
+++ src/test/java/com/httpbin/stepdefinitions/Status_code.java
@@ -1,4 +1,5 @@
 package com.httpbin.stepdefinitions;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -11,18 +12,23 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.lessThan;
 
 import io.cucumber.datatable.DataTable;
+
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 import com.httpbin.utils.ConfigReader;
+import com.httpbin.utils.ExcelUtility;
 
 public class Status_code {
 
-    private Response response;
-    private int chainedStatusCode;
+    Response response;
+    int chainedStatusCode;
+    ExcelUtility eUtil = new ExcelUtility();
 
     @Given("the base url of httpbin")
     public void set_base_url() {
-        ConfigReader.loadConfig();   // load properties
+        ConfigReader.loadConfig();
         baseURI = ConfigReader.get("base_url") + "/status";
     }
 
@@ -76,8 +82,12 @@ public class Status_code {
                 .patch("/");
     }
 
-    @When("the delete request is sent with status code {int}")
-    public void sent_delete_request(int code) {
+    @When("the delete request is sent using excel data {string}")
+    public void sent_delete_request_excel(String sheetName) throws IOException {
+
+        List<Map<String, String>> data = eUtil.getSheetData(sheetName);
+        int code = Integer.parseInt(data.get(0).values().iterator().next());
+
         response = given()
                 .auth().basic(
                         ConfigReader.get("username"),
