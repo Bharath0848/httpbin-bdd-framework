972491f7ecf06abc14ce15e0b6a2e4c1cb83582c
Arthi <arthiraje23@gmail.com>
2026-04-22 08:15:57 +0530

    Added Scenario Outline and Data Table for HttpMethods

--- src/test/java/com/httpbin/stepdefinitions/Step_Def_HttpMethods.java
+++ src/test/java/com/httpbin/stepdefinitions/Step_Def_HttpMethods.java
@@ -1,7 +1,10 @@
 package com.httpbin.stepdefinitions;
 
 import io.cucumber.java.en.*;
+import io.cucumber.datatable.DataTable;
 import io.restassured.response.Response;
+
+import java.util.*;
 
 import static io.restassured.RestAssured.*;
 import static org.testng.Assert.*;
@@ -11,91 +14,118 @@ public class Step_Def_HttpMethods {
     String baseUrl;
     Response response;
 
-  
-    @Given("I set base URL")
-    public void setBaseUrl() {
-        baseUrl = "https://httpbin.org";
+    String name;
+    String role;
+
+ 
+    @Given("HTTPBin base URL is set to {string}")
+    public void setBaseUrl(String url) {
+        baseUrl = url;
     }
 
-    @When("I send GET request with valid query params")
-    public void sendGetRequest() {
-
+ 
+    @When("user sends GET request to {string}")
+    public void sendGetRequest(String endpoint) {
         response = given()
-                .queryParam("name", "arthi")
-                .queryParam("role", "tester")
                 .when()
-                .get(baseUrl + "/get");
+                .get(baseUrl + endpoint);
     }
 
-    @Then("I validate GET response")
-    public void validateGet() {
-        assertEquals(response.getStatusCode(), 200);
+    @Then("response args will have name {string} and role {string}")
+    public void validateGetResponse(String expName, String expRole) {
+
+        assertEquals(response.jsonPath().getString("args.name"), expName);
+        assertEquals(response.jsonPath().getString("args.role"), expRole);
     }
 
+    @When("user sends POST request with below data")
+    public void sendPostRequest(DataTable dataTable) {
 
-    @When("I send POST request with valid JSON")
-    public void sendPost() {
+        List<Map<String, String>> data = dataTable.asMaps();
 
-        String body = "{ \"project\": \"API Testing\", \"status\": \"running\" }";
+        Map<String, String> body = new HashMap<>(data.get(0));
 
         response = given()
                 .header("Content-Type", "application/json")
                 .body(body)
                 .when()
                 .post(baseUrl + "/post");
-    }
 
-    @Then("I validate POST response")
-    public void validatePost() {
         assertEquals(response.getStatusCode(), 200);
+
+
+        name = response.jsonPath().getString("json.name");
+        role = response.jsonPath().getString("json.role");
     }
 
-  
-    @When("I send PUT request with valid data")
-    public void sendPut() {
+    @Then("response json will match sent payload")
+    public void validatePostResponse() {
+        assertNotNull(response.jsonPath().get("json"));
+    }
 
-        String body = "{ \"task\": \"update profile\", \"status\": \"completed\" }";
+
+    @When("user sends PUT request to {string} with name {string} and role {string}")
+    public void sendPutRequest(String endpoint, String inputName, String inputRole) {
+
+       
+        if (inputName != null) name = inputName;
+        if (inputRole != null) role = inputRole;
+
+        Map<String, String> body = new HashMap<>();
+        body.put("name", name);
+        body.put("role", role);
 
         response = given()
                 .header("Content-Type", "application/json")
                 .body(body)
                 .when()
-                .put(baseUrl + "/put");
+                .put(baseUrl + endpoint);
     }
 
-    @Then("I validate PUT response")
-    public void validatePut() {
-        assertEquals(response.getStatusCode(), 200);
+    @Then("response json will reflect role {string}")
+    public void validatePutResponse(String expectedRole) {
+
+        assertEquals(response.jsonPath().getString("json.role"), expectedRole);
     }
 
-    @When("I send PATCH request with invalid data")
-    public void sendPatch() {
 
-        String body = "{ \"invalid\": \"data\" }";
+    @When("user sends PATCH request to {string} with invalid data")
+    public void sendPatchRequest(String endpoint) {
+
+        String body = "{ invalid json }";
 
         response = given()
                 .header("Content-Type", "application/json")
                 .body(body)
                 .when()
-                .patch(baseUrl + "/patch");
+                .patch(baseUrl + endpoint);
     }
 
-    @Then("I validate PATCH response")
-    public void validatePatch() {
-        assertEquals(response.getStatusCode(), 200);
+    @Then("response will handle invalid input")
+    public void validatePatchResponse() {
+        assertTrue(response.getStatusCode() >= 200);
     }
 
-    @When("I send DELETE request with valid params")
-    public void sendDelete() {
+
+    @When("user sends DELETE request to {string}")
+    public void sendDeleteRequest(String endpoint) {
 
         response = given()
-                .queryParam("id", "101")
                 .when()
-                .delete(baseUrl + "/delete");
+                .delete(baseUrl + endpoint);
     }
 
-    @Then("I validate DELETE response")
-    public void validateDelete() {
-        assertEquals(response.getStatusCode(), 200);
+    @Then("response args will contain {string} and {string}")
+    public void validateDeleteResponse(String expName, String expRole) {
+
+        String res = response.getBody().asString();
+
+        assertTrue(res.contains(expName));
+        assertTrue(res.contains(expRole));
+    }
+
+    @Then("response time will be less than {int} ms")
+    public void validateResponseTime(int time) {
+        assertTrue(response.getTime() < time);
     }
 }
