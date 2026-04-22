82c5cbaf737f11d5479d4a3d33b870bc5d1d75ed
Mahitha <smgmm2004@gmail.com>
2026-04-22 08:18:08 +0530

    Added scenario outline, data table, and chaining for status code module

--- src/test/java/com/httpbin/stepdefinitions/Status_code.java
+++ src/test/java/com/httpbin/stepdefinitions/Status_code.java
@@ -10,75 +10,88 @@ import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.anyOf;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.lessThan;
+import io.cucumber.datatable.DataTable;
+import java.util.List;
 
 public class Status_code {
-	Response response;
-	@Given("the base url of httpbin")
-	public void set_base_url() {
-	       baseURI="https://httpbin.org/status";
-	}
-	@When("the post request is sent with status code {int}")
-	public void sent_post_request(int code) {
-	    response = given()
-	            .auth().oauth2("mytoken")
-	            .contentType(ContentType.JSON)
-	            .body("{\r\n"
-	            		+ "    \"post\":\"applied\"\r\n"
-	            		+ "}")
-	            .pathParam("code", code)
-	    .when()
-	            .post("/{code}");
-	}
-	@When("the get request is sent with status code {int}")
-	public void sent_get_request(int code) {
-	    response = given()
-	            .auth().basic("admin", "admin@123")
-	            .pathParam("code", code)
-	    .when()
-	            .get("/{code}");
-	}
-	@When("the put request is sent with status codes {string}")
-	public void sent_put_request(String codes) {
-	    response = given()
-	            .auth().oauth2("mytoken")
-	            .contentType(ContentType.JSON)
-	            .body("{\r\n"
-	            		+ "    \"put\":\"applied\"\r\n"
-	            		+ "}")
-	            .pathParam("codes", codes)
-	    .when()
-	            .put("/{codes}");
-	}
-	@When("the patch request is sent without status code")
-	public void sent_patch_request() {
-	    response = given()
-	            .auth().oauth2("mytoken")
-	            .contentType(ContentType.JSON)
-	            .body("{\r\n"
-	            		+ "    \"patch\":\"applied\"\r\n"
-	            		+ "}")
-	    .when()
-	            .patch("/");
-	}
-	@When("the delete request is sent with status code {int}")
-	public void sent_delete_request(int code) {
-	    response = given()
-	            .auth().basic("admin", "admin@123")
-	            .pathParam("code", code)
-	    .when()
-	            .delete("/{code}");
-	}
-	@Then("we get the response code as {int}")
-	public void validate_status_code(int code) {
-		response.then().statusCode(code);
-	}
-	@Then("we get the response code as either 200 or 500")
-	public void validate_multiple_status_code() {
-	    response.then().statusCode(anyOf(is(200), is(500)));
-	}
-	@Then("the response time is less than {int} ms")
-	public void validate_reponse_time(int time) {
-		response.then().time(lessThan((long) time));
-	}
 
+    private Response response;
+    private int chainedStatusCode;
+
+    @Given("the base url of httpbin")
+    public void set_base_url() {
+        baseURI = "https://httpbin.org/status";
+    }
+
+    @When("the post request is sent with status code {int}")
+    public void sent_post_request(int code) {
+        response = given()
+                .auth().oauth2("mytoken")
+                .contentType(ContentType.JSON)
+                .body("{\"post\":\"applied\"}")
+                .pathParam("code", code)
+        .when()
+                .post("/{code}");
+    }
+
+    @When("the get request is sent with status code {int}")
+    public void sent_get_request(int code) {
+        response = given()
+                .auth().basic("admin", "admin@123")
+                .pathParam("code", code)
+        .when()
+                .get("/{code}");
+
+        chainedStatusCode = response.getStatusCode();
+       
+    }
+
+    @When("the put request is sent with chained status code and below data")
+    public void sent_put_request_with_table(DataTable table) {
+        List<List<String>> data = table.cells();
+        int secondCode = Integer.parseInt(data.get(1).get(0));
+
+        String codes = chainedStatusCode + "," + secondCode;
+
+        response = given()
+                .auth().oauth2("mytoken")
+                .contentType(ContentType.JSON)
+                .body("{\"put\":\"applied\"}")
+                .pathParam("codes", codes)
+        .when()
+                .put("/{codes}");
+    }
+    @When("the patch request is sent without status code")
+    public void sent_patch_request() {
+        response = given()
+                .auth().oauth2("mytoken")
+                .contentType(ContentType.JSON)
+                .body("{\"patch\":\"applied\"}")
+        .when()
+                .patch("/");
+    }
+
+    @When("the delete request is sent with status code {int}")
+    public void sent_delete_request(int code) {
+        response = given()
+                .auth().basic("admin", "admin@123")
+                .pathParam("code", code)
+        .when()
+                .delete("/{code}");
+    }
+
+    @Then("we get the response code as {int}")
+    public void validate_status_code(int code) {
+        response.then().statusCode(code);
+    }
+
+    @Then("we get the response code as either chained status code or {int}")
+    public void validate_multiple_status_code(int secondCode) {
+        response.then().statusCode(anyOf(is(chainedStatusCode), is(secondCode)));
+    }
+
+    @Then("the response time is less than {int} ms")
+    public void validate_response_time(int time) {
+        response.then().time(lessThan((long) time));
+    }
 }
--- src/test/resources/features/Status_code.feature
+++ src/test/resources/features/Status_code.feature
@@ -1,29 +1,37 @@
-
+#Author:Mahitha
 Feature: to perform CRUD operations on HttpBin StatusCode module
+
 Background:
-Given the base url of httpbin
+  Given the base url of httpbin
 
 Scenario: Verify POST status with valid code
-When the post request is sent with status code 404
-Then we get the response code as 404
-And the response time is less than 3000 ms
+  When the post request is sent with status code 404
+  Then we get the response code as 404
+  And the response time is less than 4000 ms
 
-Scenario: Verify GET status with valid code
-When the get request is sent with status code 200
-Then we get the response code as 200
-And the response time is less than 3000 ms
+Scenario Outline: Verify GET status with valid code
+  When the get request is sent with status code <code>
+  Then we get the response code as <code>
+  And the response time is less than 4000 ms
+
+Examples:
+  | code |
+  | 200  |
 
 Scenario: Verify PUT status with multiple codes
-When the put request is sent with status codes "200,500"
-Then we get the response code as either 200 or 500
-And the response time is less than 3000 ms
+  When the get request is sent with status code 200
+  And the put request is sent with chained status code and below data
+    | secondCode |
+    | 500        |
+  Then we get the response code as either chained status code or 500
+  And the response time is less than 4000 ms
 
 Scenario: Verify PATCH status with invalid code
-When the patch request is sent without status code
-Then we get the response code as 404
-And the response time is less than 3000 ms
+  When the patch request is sent without status code
+  Then we get the response code as 404
+  And the response time is less than 4000 ms
 
 Scenario: Verify DELETE status with no content code
-When the delete request is sent with status code 204
-Then we get the response code as 204
-And the response time is less than 3000 ms
+  When the delete request is sent with status code 204
+  Then we get the response code as 204
+  And the response time is less than 4000 ms
