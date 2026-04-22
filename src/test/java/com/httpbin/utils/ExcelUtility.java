20da3eda389016bf97dc4b7245a662243e25c950
Bharath V <bharath0848t@gmail.com>
2026-04-22 13:34:31 +0530

    Excel utility class added and excel test data added into the main branch

---
+++ src/test/java/com/httpbin/utils/ExcelUtility.java
@@ -0,0 +1,50 @@
+package com.httpbin.utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelUtility {
+
+    private final String filePath = "./src/test/resources/ExcelFiles/data.xlsx";
+
+    public String getCellDataByKey(String sheetName, String key, int valueColumn) throws IOException {
+
+        try (FileInputStream fis = new FileInputStream(filePath);
+             Workbook workbook = new XSSFWorkbook(fis)) {
+
+            Sheet sheet = workbook.getSheet(sheetName);
+
+            if (sheet == null) {
+                throw new RuntimeException("Sheet not found: " + sheetName);
+            }
+
+            DataFormatter formatter = new DataFormatter();
+
+            for (int i = 0; i <= sheet.getLastRowNum(); i++) {
+                Row row = sheet.getRow(i);
+
+                if (row != null && row.getCell(0) != null) {
+                    String cellKey = formatter.formatCellValue(row.getCell(0));
+
+                    if (key.equalsIgnoreCase(cellKey)) {
+                        if (row.getCell(valueColumn) != null) {
+                            return formatter.formatCellValue(row.getCell(valueColumn));
+                        } else {
+                            return null;
+                        }
+                    }
+                }
+            }
+
+            throw new RuntimeException("Key not found in sheet: " + key);
+        }
+    }
+}
+
+
